5670840d014f2faf5bfa648261e7b647ba44d5f3
Tran Minh Luan <15520454@gm.uit.edu.vn>
2017-09-12 22:41:30 +0700

    Finished day 3

--- day2/script.js
+++ day2/script.js
@@ -1,4 +1,5 @@
 function everySecond () {
+	const allHands = document.querySelectorAll('.hand');
 	const secondHand = document.querySelector('.hand.second-hand');
 	const minHand = document.querySelector('.hand.min-hand');
 	const hourHand = document.querySelector('.hand.hour-hand');
@@ -15,8 +16,13 @@ function everySecond () {
 	const hours = now.getHours();
 	const hourDegrees = hours*30 + 90;
 	hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+		if(secondDegrees === 90) {
+    		allHands.forEach(hand =>hand.style.transition = 'none');
+		} else {
+    		allHands.forEach(hand =>hand.style.transition = '');
+    	}
 	
 }
 
 setInterval(everySecond, 1000);
-
